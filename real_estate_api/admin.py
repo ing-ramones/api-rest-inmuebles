886f0183e911e61f33922d793bfa69daa8731a47
Lumar <lumaramones@gmail.com>
2020-11-29 23:05:48 -0300

    Se modifico el admin un poco para mostrar los campos de los modelos

--- real_estate_api/admin.py
+++ real_estate_api/admin.py
@@ -6,5 +6,19 @@ admin.site.site_header = "Welcome to the administration site"
 admin.site.site_title = "Real Estate Admin"
 admin.site.index_title = "Inventory"
 
-admin.site.register(models.Estate)
-admin.site.register(models.Company)
+
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'address', 'area',
+                    'rooms', 'garage', 'geom', 'other', 'company_id',)
+
+    readonly_fields = ['created_by_id', 'last_modified_by_id']
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'nif',)
+
+    readonly_fields = ['created_by_id', 'last_modified_by_id']
+
+
+admin.site.register(models.Estate, EstateAdmin)
+admin.site.register(models.Company, CompanyAdmin)
---
+++ real_estate_api/test/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class RealEstatesTestClass(TestCase):
+
+    def setUpTestData(cls) -> None:
+        ...
+
+    def setUp(self) -> None:
+        ...
