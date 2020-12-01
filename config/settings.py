1d1c6e34767ad482780df81c82efeb7f3ecf8c69
Lumar <lumaramones@gmail.com>
2020-12-01 03:31:44 -0300

    Reconfiguracon archivo settings 7

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l4%3$v=kz(tcez7m9syh0@4^(u72y4%vx30x%+y3!t-7$+6ev@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
