78efe91fa160cbc21cd630d0d7c7e0079f3be8f9
Lumar <lumaramones@gmail.com>
2020-12-01 00:37:16 -0300

    Ups..

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -93,7 +93,7 @@ DATABASES = {
         'PORT': '5434',
     }
 }
-"""
+
 
 #db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
