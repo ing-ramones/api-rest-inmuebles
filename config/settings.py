0b185dc52773de40823bed29fab5c46ec8935ec4
Lumar <lumaramones@gmail.com>
2020-12-01 00:00:04 -0300

    Configuracion activos estaticos

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
@@ -143,3 +145,8 @@ REST_FRAMEWORK = {
     ],
     'EXCEPTION_HANDLER': 'app.utils.custom_exception_handler',
 }
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
