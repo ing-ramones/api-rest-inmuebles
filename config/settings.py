d01f5d94c81b659750cb60ccb1b9668af737ecd3
Lumar <lumaramones@gmail.com>
2020-12-01 02:51:59 -0300

    Reconfiguracon archivo settings 6

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = '{{ project_name }}.config.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = '{{ project_name }}.config.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
-    'EXCEPTION_HANDLER': '{{ project_name }}.utils.custom_exception_handler',
+    'EXCEPTION_HANDLER': 'app.utils.custom_exception_handler',
 }
 
 # Extra places for collectstatic to find static files.
